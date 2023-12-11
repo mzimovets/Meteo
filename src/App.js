38703dcddd6c1ea5dc0e298c6d20735bf57dc56c
maks <zimovets3@yandex.ru>
2023-12-11 16:25:08 +0300

    Meteo-beta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,94 @@
+import { useState } from "react";
 import "./App.css";
 import { Meteo } from "./components/Meteo";
+import { useEffect } from "react";
+import mqtt from "mqtt";
 
 function App() {
+  const [client, setClient] = useState(null);
+  const [connectionStatus, setConnectStatus] = useState("");
+  const [mqttData, setMqttData] = useState();
+  const mqttConnect = (mqttOption) => {
+    setConnectStatus("Connecting");
+    setClient(mqtt.connect("ws://test.mosquitto.org", mqttOption));
+  };
+
+  useEffect(() => {
+    mqttConnect({
+      host: "test.mosquitto.org", //"195.161.68.19",
+      port: 8080,
+      protocol: "ws",
+      clientId: "max1" + new Date(),
+    });
+  }, []);
+
+  useEffect(() => {
+    // if (connectStatus !== "Connected") {
+    //   mqttConnect({
+    //     host: "192.168.1.96",
+    //     port: 8888,
+    //     protocol: "ws",
+    //     clientId: "asdg1",
+    //   });
+    // }
+    if (client) {
+      console.log(client);
+      client.on("connect", () => {
+        setConnectStatus("Connected");
+        mqttSub({ topic: "weather-station", qos: 1 });
+
+        console.log("mqtt connect");
+        // const { topic, qos, payload } = context;
+      });
+      client.on("error", (err) => {
+        console.error("Connection error: ", err);
+        // client.end();
+      });
+      client.on("reconnect", () => {
+        console.log("mqtt reconnect");
+        setConnectStatus("Reconnecting");
+      });
+      client.on("message", (topic, message) => {
+        const payload = { topic, message: message.toString() };
+        console.log("mqtt message", payload);
+        // setPayload(payload);
+        console.log("payload", payload);
+        setMqttData(message.toString());
+        message.toString().split(" ");
+        const paramValue = message.toString().split(" ");
+      });
+    }
+  }, [client]);
+
+  const mqttSub = (subscription) => {
+    if (client) {
+      const { topic, qos } = subscription;
+      client.subscribe(topic, { qos }, (error) => {
+        if (error) {
+          console.log("Subscribe to topics error", error);
+          return;
+        }
+        console.log("mqtt subscribe at", subscription);
+        // setIsSub(true);
+      });
+    }
+  };
+
+  const mqttPublish = (context) => {
+    if (client) {
+      const { topic, qos, payload } = context;
+      client.publish(topic, payload, { qos }, (error) => {
+        if (error) {
+          console.log("Publish error: ", error);
+        }
+      });
+    }
+  };
+
   return (
     <div>
-      <Meteo />
+      {connectionStatus}
+      <Meteo mqttData={mqttData} />
     </div>
   );
 }
--- src/components/Meteo.js
+++ src/components/Meteo.js
@@ -1,4 +1,30 @@
-const Meteo = () => {
+import { useEffect, useState } from "react";
+
+const Meteo = (props) => {
+  const [tempIn, setTempIn] = useState("-");
+  const [tempOut, setTempOut] = useState("-");
+  const [humidity, setHumidity] = useState("-");
+  const [barPressure, setBarPressure] = useState("-");
+
+  useEffect(() => {
+    prepareMqttData();
+  }, [props.mqttData]);
+
+  const prepareMqttData = () => {
+    const paramValue = props.mqttData?.split(" ");
+    if (paramValue?.length === 2) {
+      if (paramValue[0] === "ti") {
+        setTempIn(paramValue[1]);
+      } else if (paramValue[0] === "to") {
+        setTempOut(paramValue[1]);
+      } else if (paramValue[0] === "hi") {
+        setHumidity(paramValue[1]);
+      } else {
+        setBarPressure(paramValue[1]);
+      }
+    }
+  };
+
   return (
     <>
       <div className="font-serif">
@@ -7,16 +33,19 @@ const Meteo = () => {
           <div style={{ display: "flex" }}>
             <div className="indicators-cards">
               <div className="cards-title">Температура внутри</div>
+              <div>{tempIn}°</div>
             </div>
             <div className="indicators-cards">
               <div className="cards-title">Температура снаружи</div>
+              <div>{tempOut}°</div>
             </div>
             <div className="indicators-cards">
               <div className="cards-title">Влажность</div>
-              <img src="bublic/dry.png"></img>
+              <div>{humidity}%</div>
             </div>
             <div className="indicators-cards">
               <div className="cards-title">Давление</div>
+              <div>{barPressure}</div>
             </div>
           </div>
         </div>
